db2f2e9d557e02577174a81aa9e057b0ed24f31f
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-09-08 13:32:35 +0545

    updated to one to one mapping using embadding

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService:UserService){}
+
+    @Post()
+    create(){
+        return this.userService.createUser();
+    }
+
+    @Get()
+    getUsers(){
+        return this.userService.findAll()
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,26 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectModel(User.name) private userModel: Model<User>){}
+
+    async createUser():Promise<User>{
+        const user = new this.userModel({
+
+            name: "Naresh",
+            address:{
+                street: "mohanpur",
+                city:"dhangadhi"
+            }
+        })
+        return user.save();
+    }
+
+    async findAll():Promise<User[]>{
+        return this.userModel.find();
+    }
+}
