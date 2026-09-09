f6db1bc02fac4990bf930b91d936cd3244cc5fa0
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-09-09 16:11:49 +0545

    updated upto many-to-many relationship

---
+++ src/project/project.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+import { ProjectService } from './project.service';
+
+@Controller('project')
+export class ProjectController {
+  constructor(private readonly projectService: ProjectService) {}
+
+  @Post('seed')
+  seed() {
+    return this.projectService.seed();
+  }
+
+  @Get('developers')
+  getDevelopers() {
+    return this.projectService.getDevelopers();
+  }
+
+  @Get('projects')
+  getProjects() {
+    return this.projectService.getProjects();
+  }
+}
